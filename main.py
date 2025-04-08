73d36dbfab0c2b0f3783a121254cf47469fd1278
Hsch22 <1765334532@qq.com>
2025-04-08 13:39:23 +0800

    精简项目架构

--- main.py
+++ main.py
@@ -28,10 +28,6 @@ def main():
     from core_functional import get_core_functions
     functional = get_core_functions()
 
-    # 高级函数插件
-    from crazy_functional import get_crazy_functions
-    crazy_fns = get_crazy_functions()
-
     # 处理markdown文本格式的转变
     gr.Chatbot.postprocess = format_io
 
@@ -51,136 +47,19 @@ def main():
         CHATBOT_HEIGHT /= 2
 
     cancel_handles = []
-    with gr.Blocks(title="ChatGPT 学术优化", theme=set_theme, analytics_enabled=False, css=advanced_css) as demo:
-        gr.HTML(title_html)
-        cookies = gr.State({'api_key': API_KEY, 'llm_model': LLM_MODEL})
-        with gr_L1():
-            with gr_L2(scale=2):
-                chatbot = gr.Chatbot()
-                chatbot.style(height=CHATBOT_HEIGHT)
-                history = gr.State([])
-            with gr_L2(scale=1):
-                with gr.Accordion("输入区", open=True) as area_input_primary:
-                    with gr.Row():
-                        txt = gr.Textbox(show_label=False, placeholder="Input question here.").style(container=False)
-                    with gr.Row():
-                        submitBtn = gr.Button("提交", variant="primary")
-                    with gr.Row():
-                        resetBtn = gr.Button("重置", variant="secondary"); resetBtn.style(size="sm")
-                        stopBtn = gr.Button("停止", variant="secondary"); stopBtn.style(size="sm")
-                        clearBtn = gr.Button("清除", variant="secondary", visible=False); clearBtn.style(size="sm")
-                    with gr.Row():
-                        status = gr.Markdown(f"Tip: 按Enter提交, 按Shift+Enter换行。当前模型: {LLM_MODEL} \n {proxy_info}")
-                with gr.Accordion("基础功能区", open=True) as area_basic_fn:
-                    with gr.Row():
-                        for k in functional:
-                            variant = functional[k]["Color"] if "Color" in functional[k] else "secondary"
-                            functional[k]["Button"] = gr.Button(k, variant=variant)
-                with gr.Accordion("函数插件区", open=True) as area_crazy_fn:
-                    with gr.Row():
-                        gr.Markdown("注意：以下“红颜色”标识的函数插件需从输入区读取路径作为参数.")
-                    with gr.Row():
-                        for k in crazy_fns:
-                            if not crazy_fns[k].get("AsButton", True): continue
-                            variant = crazy_fns[k]["Color"] if "Color" in crazy_fns[k] else "secondary"
-                            crazy_fns[k]["Button"] = gr.Button(k, variant=variant)
-                            crazy_fns[k]["Button"].style(size="sm")
-                    with gr.Row():
-                        with gr.Accordion("更多函数插件", open=True):
-                            dropdown_fn_list = [k for k in crazy_fns.keys() if not crazy_fns[k].get("AsButton", True)]
-                            with gr.Column(scale=1):
-                                dropdown = gr.Dropdown(dropdown_fn_list, value=r"打开插件列表", label="").style(container=False)
-                            with gr.Column(scale=1):
-                                switchy_bt = gr.Button(r"请先从插件列表中选择", variant="secondary")
-                    with gr.Row():
-                        with gr.Accordion("点击展开“文件上传区”。上传本地文件可供红色函数插件调用。", open=False) as area_file_up:
-                            file_upload = gr.Files(label="任何文件, 但推荐上传压缩文件(zip, tar)", file_count="multiple")
-                with gr.Accordion("更换模型 & SysPrompt & 交互界面布局", open=(LAYOUT == "TOP-DOWN")):
-                    system_prompt = gr.Textbox(show_label=True, placeholder=f"System Prompt", label="System prompt", value=initial_prompt)
-                    top_p = gr.Slider(minimum=-0, maximum=1.0, value=1.0, step=0.01,interactive=True, label="Top-p (nucleus sampling)",)
-                    temperature = gr.Slider(minimum=-0, maximum=2.0, value=1.0, step=0.01, interactive=True, label="Temperature",)
-                    max_length_sl = gr.Slider(minimum=256, maximum=4096, value=512, step=1, interactive=True, label="Local LLM MaxLength",)
-                    checkboxes = gr.CheckboxGroup(["基础功能区", "函数插件区", "底部输入区", "输入清除键"], value=["基础功能区", "函数插件区"], label="显示/隐藏功能区")
-                    md_dropdown = gr.Dropdown(AVAIL_LLM_MODELS, value=LLM_MODEL, label="更换LLM模型/请求源").style(container=False)
-
-                    gr.Markdown(description)
-                with gr.Accordion("备选输入区", open=True, visible=False) as area_input_secondary:
-                    with gr.Row():
-                        txt2 = gr.Textbox(show_label=False, placeholder="Input question here.", label="输入区2").style(container=False)
-                    with gr.Row():
-                        submitBtn2 = gr.Button("提交", variant="primary")
-                    with gr.Row():
-                        resetBtn2 = gr.Button("重置", variant="secondary"); resetBtn2.style(size="sm")
-                        stopBtn2 = gr.Button("停止", variant="secondary"); stopBtn2.style(size="sm")
-                        clearBtn2 = gr.Button("清除", variant="secondary", visible=False); clearBtn.style(size="sm")
-        # 功能区显示开关与功能区的互动
-        def fn_area_visibility(a):
-            ret = {}
-            ret.update({area_basic_fn: gr.update(visible=("基础功能区" in a))})
-            ret.update({area_crazy_fn: gr.update(visible=("函数插件区" in a))})
-            ret.update({area_input_primary: gr.update(visible=("底部输入区" not in a))})
-            ret.update({area_input_secondary: gr.update(visible=("底部输入区" in a))})
-            ret.update({clearBtn: gr.update(visible=("输入清除键" in a))})
-            ret.update({clearBtn2: gr.update(visible=("输入清除键" in a))})
-            if "底部输入区" in a: ret.update({txt: gr.update(value="")})
-            return ret
-        checkboxes.select(fn_area_visibility, [checkboxes], [area_basic_fn, area_crazy_fn, area_input_primary, area_input_secondary, txt, txt2, clearBtn, clearBtn2] )
-        # 整理反复出现的控件句柄组合
-        input_combo = [cookies, max_length_sl, md_dropdown, txt, txt2, top_p, temperature, chatbot, history, system_prompt]
-        output_combo = [cookies, chatbot, history, status]
-        predict_args = dict(fn=ArgsGeneralWrapper(predict), inputs=input_combo, outputs=output_combo)
-        # 提交按钮、重置按钮
-        cancel_handles.append(txt.submit(**predict_args))
-        cancel_handles.append(txt2.submit(**predict_args))
-        cancel_handles.append(submitBtn.click(**predict_args))
-        cancel_handles.append(submitBtn2.click(**predict_args))
-        resetBtn.click(lambda: ([], [], "已重置"), None, [chatbot, history, status])
-        resetBtn2.click(lambda: ([], [], "已重置"), None, [chatbot, history, status])
-        clearBtn.click(lambda: ("",""), None, [txt, txt2])
-        clearBtn2.click(lambda: ("",""), None, [txt, txt2])
-        # 基础功能区的回调函数注册
-        for k in functional:
-            click_handle = functional[k]["Button"].click(fn=ArgsGeneralWrapper(predict), inputs=[*input_combo, gr.State(True), gr.State(k)], outputs=output_combo)
-            cancel_handles.append(click_handle)
-        # 文件上传区，接收文件后与chatbot的互动
-        file_upload.upload(on_file_uploaded, [file_upload, chatbot, txt, txt2, checkboxes], [chatbot, txt, txt2])
-        # 函数插件-固定按钮区
-        for k in crazy_fns:
-            if not crazy_fns[k].get("AsButton", True): continue
-            click_handle = crazy_fns[k]["Button"].click(ArgsGeneralWrapper(crazy_fns[k]["Function"]), [*input_combo, gr.State(PORT)], output_combo)
-            click_handle.then(on_report_generated, [file_upload, chatbot], [file_upload, chatbot])
-            cancel_handles.append(click_handle)
-        # 函数插件-下拉菜单与随变按钮的互动
-        def on_dropdown_changed(k):
-            variant = crazy_fns[k]["Color"] if "Color" in crazy_fns[k] else "secondary"
-            return {switchy_bt: gr.update(value=k, variant=variant)}
-        dropdown.select(on_dropdown_changed, [dropdown], [switchy_bt] )
-        # 随变按钮的回调函数注册
-        def route(k, *args, **kwargs):
-            if k in [r"打开插件列表", r"请先从插件列表中选择"]: return 
-            yield from ArgsGeneralWrapper(crazy_fns[k]["Function"])(*args, **kwargs)
-        click_handle = switchy_bt.click(route,[switchy_bt, *input_combo, gr.State(PORT)], output_combo)
-        click_handle.then(on_report_generated, [file_upload, chatbot], [file_upload, chatbot])
-        # def expand_file_area(file_upload, area_file_up):
-        #     if len(file_upload)>0: return {area_file_up: gr.update(open=True)}
-        # click_handle.then(expand_file_area, [file_upload, area_file_up], [area_file_up])
-        cancel_handles.append(click_handle)
-        # 终止按钮的回调函数注册
-        stopBtn.click(fn=None, inputs=None, outputs=None, cancels=cancel_handles)
-        stopBtn2.click(fn=None, inputs=None, outputs=None, cancels=cancel_handles)
 
     # gradio的inbrowser触发不太稳定，回滚代码到原始的浏览器打开函数
-    def auto_opentab_delay():
-        import threading, webbrowser, time
-        print(f"如果浏览器没有自动打开，请复制并转到以下URL：")
-        print(f"\t（亮色主题）: http://localhost:{PORT}")
-        print(f"\t（暗色主题）: http://localhost:{PORT}/?__dark-theme=true")
-        def open(): 
-            time.sleep(2)       # 打开浏览器
-            webbrowser.open_new_tab(f"http://localhost:{PORT}/?__dark-theme=true")
-        threading.Thread(target=open, name="open-browser", daemon=True).start()
-        threading.Thread(target=auto_update, name="self-upgrade", daemon=True).start()
-        threading.Thread(target=warm_up_modules, name="warm-up", daemon=True).start()
+    # def auto_opentab_delay():
+    #     import threading, webbrowser, time
+    #     print(f"如果浏览器没有自动打开，请复制并转到以下URL：")
+    #     print(f"\t（亮色主题）: http://localhost:{PORT}")
+    #     print(f"\t（暗色主题）: http://localhost:{PORT}/?__dark-theme=true")
+    #     def open(): 
+    #         time.sleep(2)       # 打开浏览器
+    #         webbrowser.open_new_tab(f"http://localhost:{PORT}/?__dark-theme=true")
+    #     threading.Thread(target=open, name="open-browser", daemon=True).start()
+    #     threading.Thread(target=auto_update, name="self-upgrade", daemon=True).start()
+    #     threading.Thread(target=warm_up_modules, name="warm-up", daemon=True).start()
 
     
     #桌面宠物的类
@@ -236,8 +115,7 @@ def main():
                 self.set_new_timers()  # 初始化停止时间和移动时间
             #快捷键监听
             self.chat_window_open = False
-            # web监听
-            self.chat_web_thread = None
+
 
 
         #初始化界面
@@ -272,14 +150,12 @@ def main():
             self.menu = QMenu(self)
             #调用gpt聊天框
             chat_action = QAction("(开发中）本地聊天", self, triggered=self.toggle_chat_window)
-            #调用web学术优化聊天框
-            web_action = QAction("学术优化", self, triggered=self.toggle_chat_web)
             change_icon_action = QAction("更换图标", self, triggered=self.change_icon)
             exit_action = QAction("退出", self, triggered=self.close)
             change_nickname_action = QAction("改昵称", self, triggered=self.change_nickname)
             settings_action = QAction("设置", self, triggered=self.show_settings_dialog)
             schedule_action = QAction("日程管理", self, triggered=self.show_schedule_dialog)
-            self.menu.addActions([chat_action, web_action, change_icon_action, change_nickname_action, settings_action, schedule_action, exit_action])
+            self.menu.addActions([chat_action, change_icon_action, change_nickname_action, settings_action, schedule_action, exit_action])
 
             #随机发起对话功能的气泡框
             self.bubble = QLabel(self)
@@ -490,57 +366,24 @@ def main():
             chat_window_shortcut_layout.addWidget(self.chat_window_shortcut_input)
             layout.addLayout(chat_window_shortcut_layout)
 
-            chat_web_shortcut_layout = QHBoxLayout()
-            chat_web_shortcut_label = QLabel("学术优化快捷键:")
-            self.chat_web_shortcut_input = QKeySequenceEdit()
-            self.chat_web_shortcut_input.setKeySequence(QKeySequence(self.config.get("Pet", "Shortcuts_CHAT_WEB")))
-            chat_web_shortcut_layout.addWidget(chat_web_shortcut_label)
-            chat_web_shortcut_layout.addWidget(self.chat_web_shortcut_input)
-            layout.addLayout(chat_web_shortcut_layout)
+
 
             ok_button = QPushButton("确定", self)
-            ok_button.clicked.connect(lambda: self.save_all_config(self.walk_checkbox.isChecked(), self.random_question_checkbox.isChecked(), self.openai_key_input.text(), self.chat_window_shortcut_input.keySequence().toString(), self.chat_web_shortcut_input.keySequence().toString()))
+            ok_button.clicked.connect(lambda: self.save_all_config(self.walk_checkbox.isChecked(), self.random_question_checkbox.isChecked(), self.openai_key_input.text(), self.chat_window_shortcut_input.keySequence().toString()))
             ok_button.clicked.connect(settings_dialog.accept)
             layout.addWidget(ok_button)
 
             settings_dialog.setLayout(layout)
             settings_dialog.exec_()
 
-        def save_all_config(self, random_walk, random_chat, openai_key, chat_window_shortcut,chat_web_shortcut):
+        def save_all_config(self, random_walk, random_chat, openai_key, chat_window_shortcut):
             self.config.set('Pet', 'RANDOM_WALK', str(random_walk))
             self.config.set('Pet', 'RANDOM_CHAT', str(random_chat))
             self.config.set("OpenAI", "openai_api_key", openai_key)
             self.config.set("Pet", "Shortcuts_CHAT_WINDOW", chat_window_shortcut)
-            self.config.set("Pet", "Shortcuts_CHAT_WEB", chat_web_shortcut)
             self.save_config()
 
-        def run_chat_web(self):
-            import asyncio
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
 
-            try:
-                auto_opentab_delay()
-                self.url = f"http://0.0.0.0:{PORT}?__dark-theme=true"
-                demo.queue(concurrency_count=CONCURRENT_COUNT).launch(
-                    server_name="0.0.0.0",
-                    server_port=PORT,
-                    auth=AUTHENTICATION,
-                )
-            finally:
-                loop.close()
-
-        def open_webpage(self):
-            import webbrowser
-            # Add your code to open the webpage here. This depends on the browser and how you want to open the webpage.
-            webbrowser.open(self.url, new=0, autoraise=True)
-
-        def toggle_chat_web(self):
-            if self.chat_web_thread is None:
-                self.chat_web_thread = threading.Thread(target=self.run_chat_web)
-                self.chat_web_thread.start()
-            else:
-                self.open_webpage()
             
 
         #快捷键打开网页版窗口
@@ -561,9 +404,7 @@ def main():
         #由于keyboard库的add_hotkey函数在主线程中运行，从而阻塞了Qt事件循环导致的。为了解决这个问题，我们可以在一个单独的线程中运行全局快捷键监听器。
         def _run_keyboard_listener(self):
             chat_window_shortcut = self.config.get("Pet", "Shortcuts_CHAT_WINDOW")
-            chat_web_shortcut = self.config.get("Pet", "Shortcuts_CHAT_WEB")
             keyboard.add_hotkey(chat_window_shortcut, lambda: QTimer.singleShot(0, pet.toggle_chat_window))
-            keyboard.add_hotkey(chat_web_shortcut, lambda: QTimer.singleShot(0, pet.toggle_chat_web))
             keyboard.wait()
             
 
